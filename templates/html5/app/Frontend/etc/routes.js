a808486bae230c982a05980a0a28c00a43c05d3a
Romain Forlot <romain.forlot@iot.bzh>
2017-05-18 17:49:35 +0200

    Git repo can be used as submodules
    Clean templates files as they are useless for usage in submodules into a project.
    
    Change-Id: I24c71b64ab2b3a958494f3f190c014227a1da576
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- templates/html5/app/Frontend/etc/routes.js
+++
@@ -1,1 +0,0 @@
-var foundationRoutes = [{"name":"SampleHome","url":"/sample-home","controller":"SampleHomeController as ctrl","animationIn":"slideInRight","path":"pages/SampleHome/SampleHome.html"}]; 
