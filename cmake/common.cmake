476aa3eb99186e27f3e0b1841d5fe823c99ed52e
Romain Forlot <romain.forlot@iot.bzh>
2017-05-28 13:36:12 +0200

    Fix missing variable use for remote debug
    Change-Id: I73a3c490e9abe780a79ac24fbfc838e5bd18eade
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -278,6 +278,9 @@ project(${PROJECT_NAME} VERSION ${PROJECT_VERSION} LANGUAGES ${PROJECT_LANGUAGES
 set(PROJECT_LIBDIR "${CMAKE_SOURCE_DIR}/libs" CACHE PATH "Subpath to libraries")
 set(PROJECT_RESOURCES "${CMAKE_SOURCE_DIR}/data" CACHE PATH "Subpath to data")
 
+set(AFB_TOKEN   ""      CACHE PATH "Default AFB_TOKEN")
+set(AFB_REMPORT "1234" CACHE PATH "Default AFB_TOKEN")
+
 INCLUDE(FindPkgConfig)
 INCLUDE(CheckIncludeFiles)
 INCLUDE(CheckLibraryExists)
