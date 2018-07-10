a7aef2878fde773eefc59423b06728415c6f17d7
Sebastien Douheret <sebastien.douheret@iot.bzh>
2018-07-10 19:28:17 +0200

    Rework CMAKE_INSTALL_PREFIX and INSTALL_PREFIX var
    Following change ID 1ec753146f, CMAKE_INSTALL_PREFIX has been
    replaced by INSTALL_PREFIX. So differ CMAKE_PREFIX_PATH and
    LD_LIBRARY_PATH setting in 03-macros in order to be sure that
    CMAKE_INSTALL_PREFIX has been set correctly.
    
    Change-Id: I6effcaaab658fd24c929cd4a30fa38ad8f4df145
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- cmake/cmake.d/03-macros.cmake
+++ cmake/cmake.d/03-macros.cmake
@@ -113,6 +113,10 @@ macro(set_install_prefix)
 		message(STATUS "-- Overwrite the CMAKE default install prefix with ${INSTALL_PREFIX}")
 		set(CMAKE_INSTALL_PREFIX ${INSTALL_PREFIX} CACHE PATH "Install prefix" FORCE)
 	endif()
+	# (BUG!!!) as PKG_CONFIG_PATH does not work [should be an env variable]
+	# ---------------------------------------------------------------------
+	set(CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX}/lib64/pkgconfig ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig CACHE PATH 'Prefix Path list used by pkgconfig module')
+	set(LD_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/lib64 ${CMAKE_INSTALL_PREFIX}/lib CACHE PATH 'Path list where to search for libraries')
 endmacro()
 
 macro(PROJECT_TARGET_ADD TARGET_NAME)
