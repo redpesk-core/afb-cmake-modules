6816718b079d3c972e94344ab585326fd0ae2c20
Jose Bollo <jose.bollo@iot.bzh>
2022-08-05 17:20:30 +0200

    Improve output of function prevent_in_source_build

--- src/cmake/cmake.d/03-macros.cmake
+++ src/cmake/cmake.d/03-macros.cmake
@@ -25,19 +25,24 @@
 #
 ###########################################################################
 
-MACRO(prevent_in_source_build)
-	execute_process(COMMAND rm -rf ${CMAKE_SOURCE_DIR}/CMakeCache.txt
-			${CMAKE_SOURCE_DIR}/CMakeCacheForScript.cmake
-			${CMAKE_SOURCE_DIR}/CMakeFiles
-			${CMAKE_SOURCE_DIR}/cmake_install.cmake)
-
-	get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
-	get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
-
-	if(${srcdir} STREQUAL ${bindir})
-		message(FATAL_ERROR "${Red}**** ERROR: You trying to build the project from the source directory or a previous build in-source occured. This isn't allowed, you have to clean CMakeCache.txt file from your source directory (${srcdir}), and build from a separate directory. ****\n")
+function(prevent_in_source_build)
+	if(EXISTS ${CMAKE_SOURCE_DIR}/CMakeCache.txt
+	 OR EXISTS ${CMAKE_SOURCE_DIR}/CMakeFiles)
+		execute_process(
+			COMMAND rm -f CMakeCacheForScript.cmake cmake_install.cmake
+			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+		message(FATAL_ERROR "\n${Red}**** ERROR: Building from the source directory isn't allowed ****\n"
+		                          " You have to build from a separate directory.\n"
+					  " Example 'mkdir build; cd build; cmake ..'\n"
+					  " Check cmake manual to get hints.\n"
+					  " \n"
+					  " But before run the below command:${Green}\n"
+					  "    rm -rf ${CMAKE_SOURCE_DIR}/CMakeFiles\n"
+					  "    rm -f ${CMAKE_SOURCE_DIR}/CMakeCache.txt\n"
+					  " ${White}\n"
+					  )
 	endif()
-ENDMACRO(prevent_in_source_build)
+endfunction(prevent_in_source_build)
 
 #--------------------------------------------------------------------------
 # CMake 3.6 imported macros to simulate list(FILTER ...) subcommand
