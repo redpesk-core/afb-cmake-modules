d8d3dab72ffa0577fb292b183f069c341e7f7009
José Bollo <jose.bollo@iot.bzh>
2023-11-13 10:02:59 +0100

    Removal of EFENCE
    Fixes #4811

--- src/cmake/cmake.d/01-x-efence.cmake
+++
@@ -1,9 +0,0 @@
-# Optional LibEfence Malloc debug library
-IF(CMAKE_BUILD_TYPE MATCHES DEBUG AND USE_EFENCE)
-	CHECK_LIBRARY_EXISTS(efence malloc "" HAVE_LIBEFENCE)
-	IF(HAVE_LIBEFENCE)
-		MESSAGE("Linking with ElectricFence for debugging purposes...")
-		SET(libefence_LIBRARIES "-lefence")
-		list (APPEND link_libraries ${libefence_LIBRARIES})
-	ENDIF(HAVE_LIBEFENCE)
-ENDIF(CMAKE_BUILD_TYPE MATCHES DEBUG AND USE_EFENCE)
